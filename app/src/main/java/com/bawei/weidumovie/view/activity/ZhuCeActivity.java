bd39e1d2e5f6b3b7435c69751a4a8f01bdbc6161
xuanran <xuan@123.com>
2019-11-06 19:31:17 +0800

    提交

---
+++ app/src/main/java/com/bawei/weidumovie/view/activity/ZhuCeActivity.java
@@ -0,0 +1,15 @@
+package com.bawei.weidumovie.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bawei.weidumovie.R;
+
+public class ZhuCeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhu_ce);
+    }
+}
