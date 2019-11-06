51f89ca2020e5fbbefb6e4dd8706f5beed2af4a5
xuanran <xuan@123.com>
2019-11-06 19:03:45 +0800

    提交

---
+++ app/src/main/java/com/bawei/weidumovie/view/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.bawei.weidumovie.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bawei.weidumovie.R;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int LayoutId() {
+        return R.layout.activity_main;
+    }
+}
