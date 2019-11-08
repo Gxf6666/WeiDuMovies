1c3daddc49d77e3f0c1d06f707a047df58a5f2b1
Gxf6666 <2769689574@qq.com>
2019-11-08 19:18:09 +0800

    定位

---
+++ app/src/main/java/com/bawei/weidumovie/testlocation/Const.java
@@ -0,0 +1,10 @@
+package com.bawei.weidumovie.testlocation;
+/*时间:2019/11/8 0008
+创建人:郭学飞*/
+
+
+public class Const {
+    public static double LONGITUDE = 0;//经度
+    public static double LATITUDE = 0;//纬度
+    public static String ADDRESS = "";//地址
+}
